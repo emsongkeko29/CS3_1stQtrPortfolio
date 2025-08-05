5737dede37bbbce697c42cb0e239bfb7a36c2939
emsongkeko29 <b2029emsongkeko@pshs.edu.ph>
2025-08-05 08:58:31 +0800

    first non graded

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
 // no script tag needed here
+
+var age = 14;
+var maxAge = 100;
+var numPerDay = 1;
+
+totalRequired= (numPerDay*365)*(maxAge - age);
+document.write("You will need " + totalRequired + " to last until the ripe old age of " +maxAge);
