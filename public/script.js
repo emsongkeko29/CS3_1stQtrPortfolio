0b8438108e390e94c3ec8327dc324b954745035e
emsongkeko29 <b2029emsongkeko@pshs.edu.ph>
2025-08-12 08:32:13 +0800

    speed test #1

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
 // no script tag needed here
 
-var age = prompt("Enter age","0")
-var maxAge = prompt("Enter maxAge", "0")
-var numPerDay = prompt("Enter the amount of times you eat your fav snacks per day", "0")
+var age = prompt("Enter age","14")
+var maxAge = prompt("Enter maxAge", "100")
+var numPerDay = prompt("Enter the amount of times you eat your fav snacks per day", "1")
 
-age = 14;
-maxAge = 100;
-numPerDay = 1;
-totalRequired = maxAge*totalAge
+var age = 14;
+var maxAge = 100;
+var numPerDay = 1;
+var totalRequired = (number(numPerDay) * 365 * (Number(maxAge) - Number(age)));
 
-let name = window.prompt("Enter", "Your name"),
+let name = window.prompt("Enter", "Your name");
 if (confirm('Are you sure?')){
     console.log("You will need" + totalRequired + "to last until the ripe age of" + maxAge);
 }
